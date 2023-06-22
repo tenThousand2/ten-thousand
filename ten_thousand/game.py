5bda0e73acf2731ad54414231ab35f5d0082c806
AnthonySinitsa <itonyemail@gmail.com>
2023-06-22 03:43:03 +0000

    asdf

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -11,56 +11,54 @@ def play(roller=None):
   if playerInput == 'n':
     print("OK. Maybe another time")
     
-  elif playerInput == "y":
-    print("""Starting round 1
-Rolling 6 dice...""")
+  else:
     start_game()
   
 def start_game():
+  print("""Starting round 1
+Rolling 6 dice...""")
+  
   total_score = 0
+  calc_points = 0
   randomRoll = dice_roller(6)
   print(f"*** {randomRoll[0]} {randomRoll[1]} {randomRoll[2]} {randomRoll[3]} {randomRoll[4]} {randomRoll[5]} ***")
   print("Enter dice to keep, or (q)uit:")
-  playerInput = input("> ")
-  if type(playerInput) == int:
-    calc_points = GameLogic.calculate_score(playerInput)
-    total_score += calc_points
-    dice_remaining = len(randomRoll) - len(playerInput)
-    print(f"You have {calc_points} unbanked points and {dice_remaining} dice remaining")
-    print("(r)oll again, (b)ank your points or (q)uit:")
+  # continue_game = True
+
+
+  while True:
+    playerInput = input("> ")
+    if playerInput == "b":
+      print(f"""You banked {calc_points} points in round 1
+  Total score is {total_score} points
+  Starting round 2
+  Rolling 6 dice...""")
+      
+      randomRoll = dice_roller(6)
+      formated_roll = format_player_input(randomRoll)
+      # print(f"*** {randomRoll[0]} {randomRoll[1]} {randomRoll[2]} {randomRoll[3]} {randomRoll[4]} {randomRoll[5]} ***")
+      print(f"*** {formated_roll} ***")
+      print("Enter dice to keep, or (q)uit:")
+    elif playerInput == "q":
+      print(f"Thanks for playing. You earned {total_score} points")
+      return -1
+
+    else:
+      intPlayerInput = format_player_input(playerInput)
+      calc_points = GameLogic.calculate_score(intPlayerInput)
+      total_score += calc_points
+      dice_remaining = len(randomRoll) - len(intPlayerInput)
+      print(f"You have {calc_points} unbanked points and {dice_remaining} dice remaining")
+      print("(r)oll again, (b)ank your points or (q)uit:")
+      playerInput = input("> ")
     
-  elif playerInput == "b":
-    print(f"""You banked {calc_points} points in round 1
-Total score is {total_score} points
-Starting round 2
-Rolling 6 dice...""")
-    
-    randomRoll = dice_roller(6)
-    print(f"*** {randomRoll[0]} {randomRoll[1]} {randomRoll[2]} {randomRoll[3]} {randomRoll[4]} {randomRoll[5]} ***")
-    print("Enter dice to keep, or (q)uit:")
-  elif playerInput == "q":
-    print(f"Thanks for playing. You earned {total_score} points")
   
+def format_player_input(player_input_string):
+  int_list = []
+  str_list = player_input_string.split()
+  for i in range(len(str_list)):
+    int_list.append(int(str_list[i]))
+    # print(f"*** {int_list} ***")
+  return int_list
 
-
-
-#   if playerInput == "5":
-#     print("You have 50 unbanked points and 5 dice remaining")
-#     print("(r)oll again, (b)ank your points or (q)uit:")
-#     playerInput = input("> ")
-#     if playerInput == "b":
-#       print("""You banked 50 points in round 1
-# Total score is 50 points
-# Starting round 2
-# Rolling 6 dice...""")
-#     # randomRoll = rolled(6)
-#     # print(f"*** {randomRoll[0]} {randomRoll[1]} {randomRoll[2]} {randomRoll[3]} {randomRoll[4]} {randomRoll[5]} ***")
-#     print("*** 6 4 5 2 3 1 ***")
-#     print("Enter dice to keep, or (q)uit:")
-#     playerInput = input("> ")
-#     if playerInput == "q":
-#       print("Thanks for playing. You earned 50 points")
-    
-  # if playerInput == "q":
-  #   print("Thanks for playing. You earned 50 points")
   
--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,53 +1,43 @@
 import random
+from collections import Counter
 
 class GameLogic:
   @staticmethod
-  def calculate_score(dice_roll):
+  def calculate_score(dice):
     score = 0
-    counts = [0] * 7 # [0, 0, 0, 0, 0, 0, 0]
-    for dice in dice_roll: # (5, 5, 5, 2, 2, 3)
-      counts[dice] += 1 # [0, 0, 0, 1, 0, 3, 0]
-      
-    if all(counts[i] == 1 for i in range(1, 7)): # 1, 2, 3, 4, 5, 6
-      score += 1500 # 1500
-      return score # 1500
-    
-    if counts[1] == 6: # 6
-      score += 4000 # 4000
-      return score # 4000
-    
-    # check for four one's
-    if counts[1] == 4: # 4
-      score += 2000 # 2000
-      return score # 2000
-    
-    if counts[1] == 5:
-      score += 3000 
-      return score 
-    
-    if counts[2] == 2:
-      if counts[3] == 2:
-        if counts[6] == 2:
-          score += 1500
-          return score
-        
-    for i in range(1, 7): # 1, 2, 3, 4, 5, 6
-      if counts[i] >= 4: # 4
-        score += i * 200 # 800
-        score += (counts[i]-4) * i * 100 # 800
-        return score # 800
-      
-    if counts[1] >= 3: # 3
-      score += 1000 # 1000
-      counts[1] -= 3 # [0, 0, 0, 1, 0, 0, 0]
-      
-    for i in range(2, 7): # 2, 3, 4, 5, 6
-      if counts[i] >= 3: # 3, 3, 3, 3, 3
-        score += i * 100 # 200, 300, 400, 500, 600
-        counts[i] -= 3 # [0, 0, 0, 0, 0, 0, 0]
-    score += counts[1] * 100 # 100
-    score += counts[5] * 50  # 150
-    return score # 1000 + 200 + 300 + 400 + 500 + 600 + 100 + 150 = 3250
+    counter = Counter(dice)
+    # Scoring for individual dice
+    score += counter[1] * 100  # Each '1' is worth 100 points
+    score += counter[5] * 50   # Each '5' is worth 50 points
+
+    # Scoring for combinations
+    for num, count in counter.items():
+        if count >= 3:  # Three or more of a kind
+            if num == 1:  # Three '1's
+                score += 1000
+            else:         # Three of any other number
+                score += num * 100
+
+            if count > 3:  # Additional dice beyond three
+                score += (count - 3) * num * 100
+
+        # Special scoring for specific combinations
+        if num == 1 or num == 5:
+            continue
+
+        if count == 4:  # Four of a kind
+            score += num * 100
+
+        if count == 5:  # Five of a kind
+            score += num * 200
+            
+        if count == 6:  # Six of a kind
+            score += num * 300
+    return score
+
+
+
+
   #    first we create a list of 7 zeros
   #    then we loop through the dice rolls and increment the count of the dice roll
   #    if the count of the dice roll is greater than or equal to 3, we add 1000 to the score
