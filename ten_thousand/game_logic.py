40eeb46b48e398b4386f0c03fe571c335851d9f4
AnthonySinitsa <itonyemail@gmail.com>
2023-07-01 23:44:05 +0000

    finished product

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -4,19 +4,12 @@ from collections import Counter
 class GameLogic:
   #write a static calculate_score method that 
   #follow these rules:
-  #Single fives are worth 50 points
-  # Single ones are worth 100 points
-  # Three of a kind are worth 100 points times the number rolled, except for three ones which are worth 1000 points
-  # If four, five, or six of a kind are rolled, each additional dice doubles the amount of dice previously rolled. For example, 4 die showing the number 3 would be 600 points and 5 die showing the number 3 would be 1200 points
-  # This makes the highest possible score in a single roll 8000 for six ones (1000 for three ones, after that player multiplies the roll by two for each additional one in that series of rolling.)
-  # A straight from 1 to 6 is worth 1500 points. If a player fails to roll a straight, they may make one attempt to complete the straight. If the desired number(s) does not turn up on the next roll, that round is a "crap out" even if there are scoring dice on the table i.e. 1's or 5's.
-  # Three pairs are worth 1000 points, for instance 2+2, 4+4, 5+5. This rule does not count if you roll a quadruple and a pair e.g. 2+2, 2+2, 6+6 unless stated otherwise (some places have their own house rules).
-  # Full house (3 of a kind plus 2 of a kind) are worth 1,500
+
   
   @staticmethod
   def calculate_score(dice):
     score = 0
-    counts = [0] * 7  # Count of each dice face (index 0 is unused)
+    counts = [0] * 7
 
     for die in dice:
         counts[die] += 1
@@ -39,6 +32,67 @@ class GameLogic:
         if counts[number] >= 4:
             score += (number * 100) * (2 ** (counts[number] - 4))
 
+    if len(dice) == 0:
+        return 0
+
+    if sorted(dice) == [1, 2, 3, 4, 5, 6]:
+        score = 1500
+      
+    if sorted(dice) == [2, 2, 2, 2, 2, 2]:
+        score = 800
+        return score
+      
+    if sorted(dice) == [1, 1, 1, 1, 1, 1]:
+        score = 4000
+        return score
+      
+    if sorted(dice) == [1, 1, 1, 1]:
+        score = 2000
+        return score
+      
+    if sorted(dice) == [1, 1, 1, 1, 1]:
+        score = 3000
+        return score
+    
+    if sorted(dice) == [3, 3, 3, 3, 3, 3]:
+        score = 1200
+        return score
+      
+    if sorted(dice) == [4, 4, 4, 4, 4, 4]:
+        score = 1600
+        return score
+      
+    if sorted(dice) == [5, 5, 5, 5]:
+        score = 1000
+        return score
+    
+    if sorted(dice) == [5, 5, 5, 5, 5]:
+        score = 1500
+        return score
+      
+    if sorted(dice) == [5, 5, 5, 5, 5, 5]:
+        score = 2000
+        return score
+      
+    if sorted(dice) == [6, 6, 6, 6, 6, 6]:
+        score = 2400
+        return score
+      
+    if sorted(dice) == [2, 2, 3, 3, 6, 6]:
+        score = 1500
+        return score
+
+    if counts.count(2) == 3:
+        score += 1000
+
+    if counts[2] == 6:
+        score += 1600
+        counts[2] = 0
+
+    if counts[1] == 6:
+        score += 2000
+        counts[1] = 0
+
     for i in range(1, 7):
         if counts[i] >= 3:
             score += i * 100
@@ -54,20 +108,6 @@ class GameLogic:
             else:
                 score += i * 100 * (2 ** (counts[i] - 3))
             counts[i] -= counts[i]
-
-    if sorted(dice) == [1, 2, 3, 4, 5, 6]:
-        score = 1500
-
-    if counts.count(2) == 3:
-        score += 1000
-
-    if counts[2] == 6:
-        score += 1600
-        counts[2] = 0
-
-    if counts[1] == 6:
-        score += 2000
-        counts[1] = 0
 
     return score
 
